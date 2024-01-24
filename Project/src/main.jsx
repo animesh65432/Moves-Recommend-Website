78f43750a3396d67de6b8f38891fff3cba79511f
animesh65432 <kiranduttta234@gmail.com>
2024-01-25 00:36:26 +0530

    Intial setup

---
+++ Project/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Body from "./Conpoments/Body/Body";
+import Footer from "./Conpoments/Footer/Footer";
+import Header from "./Conpoments/Header/Header";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode></React.StrictMode>
+);
