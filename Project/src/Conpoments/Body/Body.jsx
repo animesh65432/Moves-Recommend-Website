fd58054318a7219a9ca08e32da9fc4d77bc5a2b7
animesh65432 <kiranduttta234@gmail.com>
2024-01-25 01:10:06 +0530

    Getting Data

--- Project/src/Conpoments/Body/Body.jsx
+++ Project/src/Conpoments/Body/Body.jsx
@@ -1,5 +1,16 @@
+import { Data } from "../Data/Data";
+
 const Body = () => {
-  return <></>;
+  const Div = Data.map((obj) => (
+    <div key={obj.id}>
+      {" "}
+      <p>Name : {obj.movie}</p>
+      <img src={obj.image}></img>
+      <a href={obj.imdb_url}>Linke for</a>
+      <p>Rating : {obj.rating}</p>
+    </div>
+  ));
+  return <>{Div}</>;
 };
 
 export default Body;
--- Project/src/main.jsx
+++ Project/src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Body from "./Conpoments/Body/Body";
 import Footer from "./Conpoments/Footer/Footer";
 import Header from "./Conpoments/Header/Header";
+import Body from "./Conpoments/Body/Body";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Header />
+    <Body />
+    <Footer />
+  </React.StrictMode>
 );
