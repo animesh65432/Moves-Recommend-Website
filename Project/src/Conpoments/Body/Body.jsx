a105e8a5b0ed4481b6fa34f4a8e91780fe7d1f43
animesh65432 <kiranduttta234@gmail.com>
2024-01-25 13:35:32 +0530

    Inplement Serch Box

--- Project/src/Conpoments/Body/Body.jsx
+++ Project/src/Conpoments/Body/Body.jsx
@@ -1,20 +1,43 @@
+import React, { useState } from "react";
+
 import { Data } from "../Data/Data";
 import Card from "./Moviescard/Card";
 import "../Body/Body.css";
 
 const Body = () => {
+  const [MoviesName, SetMoviesName] = useState(Data);
+  const [FilterArray, SetFilterArray] = useState(Data);
+  const [UserInput, SetUserInput] = useState("");
+
+  const FilterArrayMethod = () => {
+    const FilterArray = MoviesName.filter((obj) =>
+      obj.movie.toLowerCase().includes(UserInput.toLocaleLowerCase())
+    );
+
+    SetFilterArray(FilterArray);
+  };
+
   return (
-    <div className="div">
-      {Data.map((obj) => (
-        <Card
-          key={obj.id}
-          image={obj.image}
-          Name={obj.movie}
-          link={obj.imdb_url}
-          rating={obj.rating}
-        />
-      ))}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          onChange={(e) => SetUserInput(e.target.value)}
+          placeholder="Please Serch Movies Here"
+        ></input>
+        <button onClick={FilterArrayMethod}>Serch</button>
+      </div>
+      <div className="div">
+        {FilterArray.map((obj) => (
+          <Card
+            key={obj.id}
+            image={obj.image}
+            Name={obj.movie}
+            link={obj.imdb_url}
+            rating={obj.rating}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
--- Project/src/Conpoments/Footer/Footer.jsx
+++ Project/src/Conpoments/Footer/Footer.jsx
@@ -1,5 +1,18 @@
-const Footer = () => {
-  return <></>;
-};
+import "../Footer/Footer.css";
+const Footer = () => (
+  <>
+    <div className="footer">
+      <div>
+        <p>CopyRight Free</p>
+      </div>
+      <p>
+        Created By{" "}
+        <a href="https://www.linkedin.com/in/animesh-dutta-ba5783273/">
+          Animesh Dutta
+        </a>
+      </p>
+    </div>
+  </>
+);
 
 export default Footer;
